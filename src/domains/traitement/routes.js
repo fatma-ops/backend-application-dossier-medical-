6225ac4943b38d7f9b4fe80d16b030695cadddb7
fatma-ops <73761826+fatma-ops@users.noreply.github.com>
2023-06-03 04:00:40 +0100

    update traitement

--- src/domains/traitement/routes.js
+++ src/domains/traitement/routes.js
@@ -150,77 +150,79 @@ router.get('/:userEmail/:idConsultation', async (req, res) => {
   });
 
 
-// update Traitement
-router.put('/update/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { userEmail, medicaments, cout, remboursement } = req.body;
-
-    if (!id) {
-      return res.status(400).json({ message: 'L\'identifiant du traitement est requis.' });
+  router.put('/update/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { userEmail } = req.body;
+      let medicaments = [];
+  
+      if (Array.isArray(req.body.medicaments)) {
+        medicaments = req.body.medicaments.map((medicament) => {
+          return {
+            nommedicament: medicament.nommedicament,
+            dateDeCommencement: medicament.dateDeCommencement,
+            nbrfois: parseInt(medicament.nbrfois),
+            nbrJours: parseInt(medicament.nbrJours),
+          };
+        });
+      }
+  
+      const cout = req.body.cout ? parseFloat(req.body.cout) : 0;
+      const remboursement = req.body.remboursement ? parseFloat(req.body.remboursement) : 0;
+  
+      if (isNaN(cout) || isNaN(remboursement)) {
+        return res.status(400).json({ message: 'Le cout et le remboursement doivent être des nombres.' });
+      }
+  
+      if (cout < 0) {
+        return res.status(400).json({ message: 'Le cout ne peut pas être négatif.' });
+      }
+  
+      if (remboursement < 0) {
+        return res.status(400).json({ message: 'Le remboursement ne peut pas être négatif.' });
+      }
+  
+      if (cout > 1000000) {
+        return res.status(400).json({ message: 'Le cout ne peut pas dépasser 1,000,000.' });
+      }
+  
+      if (remboursement > 1000000) {
+        return res.status(400).json({ message: 'Le remboursement ne peut pas dépasser 1,000,000.' });
+      }
+  
+      const updatedTraitement = {
+        cout: cout,
+        remboursement: remboursement,
+        medicaments: medicaments,
+        idConsultation: req.body.idConsultation,
+        userEmail: userEmail,
+      };
+  
+      if (medicaments.some((medicament) => medicament.nbrfois <= 0 || Number.isNaN(medicament.nbrfois) || Number.isInteger(medicament.nbrfois) === false)) {
+        return res.status(400).json({ message: 'Le nombre de fois est invalide.' });
+      }
+  
+      if (medicaments.some((medicament) => medicament.nbrJours <= 0 || Number.isNaN(medicament.nbrJours) || Number.isInteger(medicament.nbrJours) === false)) {
+        return res.status(400).json({ message: 'Le nombre de jours est invalide.' });
+      }
+  
+      if (medicaments.some((medicament) => medicament.nbrfois > 10)) {
+        return res.status(400).json({ message: 'Le nombre de fois ne doit pas dépasser 10.' });
+      }
+  
+      if (medicaments.some((medicament) => medicament.nbrJours > 30)) {
+        return res.status(400).json({ message: 'Le nombre de jours ne doit pas dépasser 30.' });
+      }
+  
+      const updatedDoc = await Traitement.findByIdAndUpdate(id, updatedTraitement, { new: true });
+      res.status(200).json(updatedDoc);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Erreur, veuillez réessayer.');
     }
-
-    
-
-    // Vérifier si le traitement existe dans la base de données
-    const existingTraitement = await Traitement.findById(id);
-    if (!existingTraitement) {
-      return res.status(404).json({ message: 'Le traitement spécifié est introuvable.' });
-    }
-
-    // Mettre à jour les propriétés du traitement existant
-    existingTraitement.userEmail = userEmail;
-    existingTraitement.medicaments = medicaments;
-    existingTraitement.cout = parseFloat(cout);
-    existingTraitement.remboursement = parseFloat(remboursement);
-
-    // Valider les modifications
-    if (isNaN(existingTraitement.cout) || isNaN(existingTraitement.remboursement)) {
-      return res.status(400).json({ message: 'Le cout et le remboursement doivent être des nombres.' });
-    }
-
-    if (existingTraitement.cout < 0) {
-      return res.status(400).json({ message: 'Le cout ne peut pas être négatif.' });
-    }
-
-    if (existingTraitement.remboursement < 0) {
-      return res.status(400).json({ message: 'Le remboursement ne peut pas être négatif.' });
-    }
-
-    if (existingTraitement.cout > 1000000) {
-      return res.status(400).json({ message: 'Le cout ne peut pas dépasser 1,000,000.' });
-    }
-
-    if (existingTraitement.remboursement > 1000000) {
-      return res.status(400).json({ message: 'Le remboursement ne peut pas dépasser 1,000,000.' });
-    }
-
-    // Valider les propriétés des médicaments
-    if (existingTraitement.medicaments.some((medicament) => medicament.nbrfois <= 0 || isNaN(medicament.nbrfois) || !Number.isInteger(medicament.nbrfois))) {
-      return res.status(400).json({ message: 'Le nombre de fois est invalide.' });
-    }
-
-    if (existingTraitement.medicaments.some((medicament) => medicament.nbrJours <= 0 || isNaN(medicament.nbrJours) || !Number.isInteger(medicament.nbrJours))) {
-      return res.status(400).json({ message: 'Le nombre de jours est invalide.' });
-    }
-
-    if (existingTraitement.medicaments.some((medicament) => medicament.nbrfois > 10)) {
-      return res.status(400).json({ message: 'Le nombre de fois ne doit pas dépasser 10.' });
-    }
-
-    if (existingTraitement.medicaments.some((medicament) => medicament.nbrJours > 30)) {
-      return res.status(400).json({ message: 'Le nombre de jours ne doit pas dépasser 30.' });
-    }
-
-    // Sauvegarder les modifications dans la base de données
-    const updatedTraitement = await existingTraitement.save();
-
-    res.status(200).json(updatedTraitement);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Erreur lors de la mise à jour du traitement.');
-  }
-});
+  });
+  
+  
 
 
 
